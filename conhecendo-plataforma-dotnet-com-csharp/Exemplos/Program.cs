9685989f34536a1a7bcce218d1b9ef618bfdf9a3
Artulanez Souza <artulanez.luz@maida.health>
2022-12-03 19:58:50 -0300

    Exemplos tipo de dados e variaveis

--- conhecendo-plataforma-dotnet-com-csharp/Exemplos/Program.cs
+++ conhecendo-plataforma-dotnet-com-csharp/Exemplos/Program.cs
@@ -1,8 +1,32 @@
 ﻿using ExemplosFundamentos.Models;
-// See https://aka.ms/new-console-template for more information
 
-Pessoa p = new Pessoa();
-p.Nome = "Artulanez Souza da Luz";
-p.Idade = 32;
+string apresentacao = "Olá, seja bem vindo";
 
-p.Apresentar();
+int quantidade = 1;
+Console.WriteLine("O valor da variável quantidade: "+ quantidade);
+quantidade = 10;
+Console.WriteLine("O valor da variável quantidade: "+ quantidade);
+
+double altura = 1.80;
+
+decimal preco = 1.80M;
+
+bool condicao = true;
+
+Console.WriteLine(apresentacao);
+// Console.WriteLine("O valor da variável quantidade: "+ quantidade);
+Console.WriteLine("O valor da variável altura: "+ altura.ToString("0.00"));
+Console.WriteLine("O valor da variável preco: "+ preco);
+Console.WriteLine("O valor da variável condicao: "+ condicao);
+
+
+DateTime dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
+
+
+
+// Pessoa p = new Pessoa();
+// p.Nome = "Artulanez Souza da Luz";
+// p.Idade = 32;
+
+// p.Apresentar();
