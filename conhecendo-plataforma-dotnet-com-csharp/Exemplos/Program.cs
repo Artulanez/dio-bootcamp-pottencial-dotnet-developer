cdf9daf178fd19281adc073a934e9a39962ea4b6
Artulanez Souza <artulanez.luz@maida.health>
2022-12-03 19:27:18 -0300

    Exemplos da Pessoa e uso da classe pessoa

---
+++ conhecendo-plataforma-dotnet-com-csharp/Exemplos/Program.cs
@@ -0,0 +1,8 @@
+﻿using ExemplosFundamentos.Models;
+// See https://aka.ms/new-console-template for more information
+
+Pessoa p = new Pessoa();
+p.Nome = "Artulanez Souza da Luz";
+p.Idade = 32;
+
+p.Apresentar();
