05cb3c1c481fd75713e8f7e7ce57ca77385eef56
Artulanez Souza <artulanez.luz@maida.health>
2022-12-03 23:06:03 -0300

    Operadores aritimeticos e cast

--- conhecendo-plataforma-dotnet-com-csharp/Exemplos/Program.cs
+++ conhecendo-plataforma-dotnet-com-csharp/Exemplos/Program.cs
@@ -1,27 +1,75 @@
 ﻿using ExemplosFundamentos.Models;
 
-string apresentacao = "Olá, seja bem vindo";
+string a = "15a";
 
-int quantidade = 1;
-Console.WriteLine("O valor da variável quantidade: "+ quantidade);
-quantidade = 10;
-Console.WriteLine("O valor da variável quantidade: "+ quantidade);
+int b = 0;
 
-double altura = 1.80;
+int.TryParse(a, out b);
 
-decimal preco = 1.80M;
+Console.WriteLine(b);
+Console.WriteLine("Conversão realizada");
 
-bool condicao = true;
 
-Console.WriteLine(apresentacao);
+
+
+
+//Ordem das operações 
+// double a = 4 / 2 + 2;
+
+// Console.WriteLine(a);
+
+
+
+
+//cast 
+// int a = 5;
+// double b = a;
+
+// Console.WriteLine(b);
+
+// int inteiro = 5;
+// string a  = inteiro.ToString();
+
+// Console.WriteLine(a);
+
+//int a = Convert.ToInt32("5");
+// int a = int.Parse("5a");
+
+// Console.WriteLine(a);
+
+// int a = 10;
+// int b = 20;
+
+// int c = a + b;
+
+// // c = c + 5;
+// c += 5;
+// Console.WriteLine(c);
+
+// Codigo criado nas aulas de tipos de dados e variáveis 
+
+// string apresentacao = "Olá, seja bem vindo";
+
+// int quantidade = 1;
 // Console.WriteLine("O valor da variável quantidade: "+ quantidade);
-Console.WriteLine("O valor da variável altura: "+ altura.ToString("0.00"));
-Console.WriteLine("O valor da variável preco: "+ preco);
-Console.WriteLine("O valor da variável condicao: "+ condicao);
+// quantidade = 10;
+// Console.WriteLine("O valor da variável quantidade: "+ quantidade);
+
+// double altura = 1.80;
+
+// decimal preco = 1.80M;
+
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// // Console.WriteLine("O valor da variável quantidade: "+ quantidade);
+// Console.WriteLine("O valor da variável altura: "+ altura.ToString("0.00"));
+// Console.WriteLine("O valor da variável preco: "+ preco);
+// Console.WriteLine("O valor da variável condicao: "+ condicao);
 
 
-DateTime dataAtual = DateTime.Now.AddDays(5);
-Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
+// DateTime dataAtual = DateTime.Now.AddDays(5);
+// Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
 
 
 
