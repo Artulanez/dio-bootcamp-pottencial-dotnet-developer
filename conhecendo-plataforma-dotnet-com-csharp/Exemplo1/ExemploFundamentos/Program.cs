6e19a4a2b1251a1bf37432f4e3bf3f4df298af4d
Artulanez Souza <artulanez.luz@maida.health>
2022-12-04 12:25:15 -0300

    trabalhando com array e lista

--- conhecendo-plataforma-dotnet-com-csharp/Exemplo1/ExemploFundamentos/Program.cs
+++ conhecendo-plataforma-dotnet-com-csharp/Exemplo1/ExemploFundamentos/Program.cs
@@ -1,7 +1,62 @@
 ﻿using ExemplosFundamentos.Common.Models;
 
-Pessoa pessoa1 = new Pessoa();
+List<string> listaString = new List<string>();
 
+listaString.Add("SP");
+listaString.Add("BA");
+listaString.Add("MG");
+listaString.Add("RJ");
+
+Console.WriteLine($"Itens na minha lista: {listaString.Count} - Capacidade: {listaString.Capacity}");
+
+listaString.Add("SC");
+
+Console.WriteLine($"Itens na minha lista: {listaString.Count} - Capacidade: {listaString.Capacity}");
+
+listaString.Remove("MG");
+
+Console.WriteLine($"Itens na minha lista: {listaString.Count} - Capacidade: {listaString.Capacity}");
+
+
+// Console.WriteLine("Percorrendo o arrey com for");
+// for (int contador = 0; contador < listaString.Count; contador++)
+// {
+//     Console.WriteLine($"Posição N°{contador} - {listaString[contador]}");
+// }
+
+// Console.WriteLine("Percorrendo o arrey com foreach");
+// int contadorForeach = 0;
+// foreach (string item in listaString)
+// {
+//     Console.WriteLine($"Posição N°{contadorForeach} - {item}");
+//     contadorForeach++;
+// }
+
+
+// Trabalahndo com Array
+// int[] arrayInteiros = new int[4];
+// arrayInteiros[0] = 72;
+// arrayInteiros[1] = 64;
+// arrayInteiros[2] = 50;
+// arrayInteiros[3] = 1;
+
+// Console.WriteLine("Percorrendo o arrey com for");
+// for (int contador = 0; contador < arrayInteiros.Length; contador++)
+// {
+//     Console.WriteLine($"Posição N°{contador} - {arrayInteiros[contador]}");
+// }
+
+// Console.WriteLine("Percorrendo o arrey com foreach");
+// int contadorForeach = 0;
+// foreach (int valor in arrayInteiros)
+// {
+//     Console.WriteLine($"Posição N°{contadorForeach} - {valor}");
+//     contadorForeach++;
+// }
+
+// int[] arrayInterirsoDobrado = new int[arrayInteiros.Length*2];
+// Array.Copy(arrayInteiros,arrayInterirsoDobrado,arrayInteiros.Length);
+//Array.Resize(ref arreyInteiros, arreyInteiros.Length *2);
 
 //estrutura de repetição
 
